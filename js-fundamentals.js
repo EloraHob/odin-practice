39205eb88fd5bb89505cf334d8d64a8d7caec065
Elora <elorahob@uw.edu>
2023-03-07 14:00:00 -0800

    learn JS basics

---
+++ js-fundamentals.js
@@ -0,0 +1,8 @@
+// Variables
+//    A "named storage" for data. Used to store various types of data.
+
+// A variable can be declared using the syntax below. Each keyword has a different purpose.
+
+var variable; // old-school way to declare a variable.
+let data; // Essentially the newer equivalent of 'var'. 
+const constant = 0;  // we will learn about this later.
