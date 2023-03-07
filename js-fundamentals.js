f32a87dd9bc5adcce63eccf56347c1db61bb9e3f
Elora <elorahob@uw.edu>
2023-03-07 14:04:43 -0800

    learn js variable types

--- js-fundamentals.js
+++ js-fundamentals.js
@@ -5,4 +5,5 @@
 
 var variable; // old-school way to declare a variable.
 let data; // Essentially the newer equivalent of 'var'. 
-const constant = 0;  // we will learn about this later.
+const constant = 0;  // a variable type that declares the variable value never changes after this first assignment. 
+                    // Usually near the top of the JS file. Naming convention is all capital-case and underscores, 'VARIABLE_NAME'.
